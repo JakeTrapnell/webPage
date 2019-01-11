a87c229c65b62e35ab3ef4294a878e1e765e7f43
JakeTrapnell <trapnelljake@gmail.com>
2019-01-11 10:34:03 +0000

    initial commit

---
+++ HTML/Hello.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Hello extends React.Component{
+    render(){
+        return 
+        <div>
+            Hello
+        </div>;
+    }
+}
+export default Hello;
